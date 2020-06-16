02c06b2749b6efad60d0dd53a160d7afa9b02ffd
yuridevops <yuri.devops@hmail.com>
2020-06-16 13:50:13 -0300

    Updates

---
+++ precache-manifest.3621d9c6acc696b6d9a85fcbc99c78e1.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0ff753649d0bb1ab3604730d1434e2b7",
+    "url": "/react-deploy/index.html"
+  },
+  {
+    "revision": "01376e48d443c839d54f",
+    "url": "/react-deploy/static/css/2.7c35e809.chunk.css"
+  },
+  {
+    "revision": "b3545dcedaaf015bb9bb",
+    "url": "/react-deploy/static/css/main.bd6b5d3c.chunk.css"
+  },
+  {
+    "revision": "01376e48d443c839d54f",
+    "url": "/react-deploy/static/js/2.93ec94de.chunk.js"
+  },
+  {
+    "revision": "cfc125643707176b9979a69345bf80d5",
+    "url": "/react-deploy/static/js/2.93ec94de.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b3545dcedaaf015bb9bb",
+    "url": "/react-deploy/static/js/main.cef11086.chunk.js"
+  },
+  {
+    "revision": "865525e4312c868c0649",
+    "url": "/react-deploy/static/js/runtime-main.ba48dd41.js"
+  },
+  {
+    "revision": "b1a540fe068b203c019d5d11a7566d4d",
+    "url": "/react-deploy/static/media/agoraip-logo.b1a540fe.png"
+  },
+  {
+    "revision": "24fb10b8e4ae499217108755b1c8fe34",
+    "url": "/react-deploy/static/media/bg-intro-blue.24fb10b8.png"
+  },
+  {
+    "revision": "a43f1711950221dbbb95bbbeec2e8105",
+    "url": "/react-deploy/static/media/bg-intro.a43f1711.jpg"
+  },
+  {
+    "revision": "68c5650b5f1d0571d9223ac97577bab2",
+    "url": "/react-deploy/static/media/conferencia.68c5650b.png"
+  },
+  {
+    "revision": "247452cb7c37c88269780a2a4aa7518e",
+    "url": "/react-deploy/static/media/dashboard.247452cb.png"
+  },
+  {
+    "revision": "c224bcbdf464da9c9412dccb96a6170e",
+    "url": "/react-deploy/static/media/devices.c224bcbd.png"
+  },
+  {
+    "revision": "e570b3c731f44f89922b6df1f6cd5114",
+    "url": "/react-deploy/static/media/duvidas.e570b3c7.png"
+  },
+  {
+    "revision": "df44e211af58a8d1b9b6bb754b044ba4",
+    "url": "/react-deploy/static/media/empresa.df44e211.jpg"
+  },
+  {
+    "revision": "8d822bc142d71d08b177df8d4de85619",
+    "url": "/react-deploy/static/media/homem-telefone.8d822bc1.png"
+  },
+  {
+    "revision": "6d9b7a3321aa598139fb056a8c2d24e1",
+    "url": "/react-deploy/static/media/ip-telephony.6d9b7a33.jpg"
+  },
+  {
+    "revision": "d806349a0f00f4a792ead3371d0a1648",
+    "url": "/react-deploy/static/media/ip-telephony.d806349a.png"
+  },
+  {
+    "revision": "5ac91566100e124debf18da3c792cdc0",
+    "url": "/react-deploy/static/media/nossa-empresa.5ac91566.png"
+  },
+  {
+    "revision": "388e82d154c80a8e478eed70403ccc30",
+    "url": "/react-deploy/static/media/pessoas-conversando.388e82d1.png"
+  },
+  {
+    "revision": "d575878d7da50814943852c39562dbaa",
+    "url": "/react-deploy/static/media/proposito.d575878d.png"
+  }
+]);
