2118f3262f3cd034f5a5c909792575d67c5dd4b4
yuridevops <yuri.devops@hmail.com>
2020-06-16 15:03:29 -0300

    Updates

---
+++ precache-manifest.87ca5703ac1796f65b9b91270920d20a.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b49c9bb92946a04226c2043f9c50900a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "01376e48d443c839d54f",
+    "url": "/static/css/2.7c35e809.chunk.css"
+  },
+  {
+    "revision": "5e4aa2241fc5ca11e34f",
+    "url": "/static/css/main.c21c0289.chunk.css"
+  },
+  {
+    "revision": "01376e48d443c839d54f",
+    "url": "/static/js/2.93ec94de.chunk.js"
+  },
+  {
+    "revision": "cfc125643707176b9979a69345bf80d5",
+    "url": "/static/js/2.93ec94de.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5e4aa2241fc5ca11e34f",
+    "url": "/static/js/main.cef11086.chunk.js"
+  },
+  {
+    "revision": "38ce07d174abd6657b71",
+    "url": "/static/js/runtime-main.2045ecef.js"
+  },
+  {
+    "revision": "b1a540fe068b203c019d5d11a7566d4d",
+    "url": "/static/media/agoraip-logo.b1a540fe.png"
+  },
+  {
+    "revision": "24fb10b8e4ae499217108755b1c8fe34",
+    "url": "/static/media/bg-intro-blue.24fb10b8.png"
+  },
+  {
+    "revision": "a43f1711950221dbbb95bbbeec2e8105",
+    "url": "/static/media/bg-intro.a43f1711.jpg"
+  },
+  {
+    "revision": "68c5650b5f1d0571d9223ac97577bab2",
+    "url": "/static/media/conferencia.68c5650b.png"
+  },
+  {
+    "revision": "247452cb7c37c88269780a2a4aa7518e",
+    "url": "/static/media/dashboard.247452cb.png"
+  },
+  {
+    "revision": "c224bcbdf464da9c9412dccb96a6170e",
+    "url": "/static/media/devices.c224bcbd.png"
+  },
+  {
+    "revision": "e570b3c731f44f89922b6df1f6cd5114",
+    "url": "/static/media/duvidas.e570b3c7.png"
+  },
+  {
+    "revision": "df44e211af58a8d1b9b6bb754b044ba4",
+    "url": "/static/media/empresa.df44e211.jpg"
+  },
+  {
+    "revision": "8d822bc142d71d08b177df8d4de85619",
+    "url": "/static/media/homem-telefone.8d822bc1.png"
+  },
+  {
+    "revision": "6d9b7a3321aa598139fb056a8c2d24e1",
+    "url": "/static/media/ip-telephony.6d9b7a33.jpg"
+  },
+  {
+    "revision": "d806349a0f00f4a792ead3371d0a1648",
+    "url": "/static/media/ip-telephony.d806349a.png"
+  },
+  {
+    "revision": "5ac91566100e124debf18da3c792cdc0",
+    "url": "/static/media/nossa-empresa.5ac91566.png"
+  },
+  {
+    "revision": "388e82d154c80a8e478eed70403ccc30",
+    "url": "/static/media/pessoas-conversando.388e82d1.png"
+  },
+  {
+    "revision": "d575878d7da50814943852c39562dbaa",
+    "url": "/static/media/proposito.d575878d.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.90665e8a12bfc969c20c61f786e5da1a.js"
+  "/precache-manifest.87ca5703ac1796f65b9b91270920d20a.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
