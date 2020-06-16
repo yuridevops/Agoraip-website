5ecb097d42f3e0060b5f70ac493f77402936551f
yuridevops <yuri.devops@hmail.com>
2020-06-16 14:45:04 -0300

    fix deploy 4

--- src/App.js
+++ src/App.js
@@ -14,10 +14,9 @@ function App() {
         <Header />
         <main>
           <Switch>
-            <Route path="/agoraip-website" exact component={Home} />
-            <Route path="/agoraip-website/sobre" component={Info} />
-            <Route path="/agoraip-website/planos" component={Plans} />
-            <Route path="/agoraip-website/planos" component={Plans} />
+            <Route path="/" exact component={Home} />
+            <Route path="/sobre" component={Info} />
+            <Route path="/planos" component={Plans} />
           </Switch>
         </main>
         <Footer />
